b78ada9433b350ebd9c85392bc3cab26750e8fe0
RainWolf0603 <49824542+RainWolf0603@users.noreply.github.com>
2021-08-22 11:23:20 +0800

    Update ChangeStatusWhilePlaying.js

--- ChangeStatusWhilePlaying.js
+++ ChangeStatusWhilePlaying.js
@@ -1,1 +1,30 @@
+/**
+ * @name ChangeStatusWhilePlaying
+ * @source https://github.com/RainWolf0603/ChangeStatusWhilePlaying/blob/main/ChangeStatusWhilePlaying.js
+ * @updateUrl https://raw.githubusercontent.com/RainWolf0603/ChangeStatusWhilePlaying/main/ChangeStatusWhilePlaying.js
+ * @website https://github.com/RainWolf0603/ChangeStatusWhilePlaying
+ * @version Beta 0.1
+ */
 
+ const config = {
+    info: {
+        name: "ChangeStatusWhilePlaying",
+        authors: [
+            {
+                name: "RainWolf",
+                discord_id: "389053119549538309",
+            }
+        ],
+        version: "Beta 0.1",
+        description: "可以在開指定遊戲時切換到指定狀態",
+        github: "https://github.com/RainWolf0603/ChangeStatusWhilePlaying",
+        github_raw: "https://raw.githubusercontent.com/RainWolf0603/ChangeStatusWhilePlaying/main/ChangeStatusWhilePlaying.js",
+    },
+    changelog: [
+        {
+            "title": "更新內容",
+            "type": "fixed",
+            "items": ["新增:  \n修復:  "]
+        }
+    ]
+};
